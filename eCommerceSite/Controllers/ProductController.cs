80c39e1f4fb4e6f167a2e2b0b7ccc12e481013e7
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-02-07 04:15:18 -0800

    Refactored DB code to async

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -20,10 +21,13 @@ namespace eCommerceSite.Controllers
         /// <summary>
         /// Displays a view that lists all products
         /// </summary>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // Get all products from database
-            List<Product> products = _context.Products.ToList();
+            // List<Product> await products = _context.Products.ToList();
+            List<Product> products =
+                await (from p in _context.Products
+                       select p).ToListAsync();
 
             // Send list of products to view to be displayed
             return View(products);
@@ -36,13 +40,13 @@ namespace eCommerceSite.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(Product p) 
+        public async Task<IActionResult> Add(Product p) 
         {
             if (ModelState.IsValid)
             {
                 // Add to DB
                 _context.Products.Add(p);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
 
                 TempData["Message"] = $"{p.ProductId}:{p.Title} was added successfully";
 
